03511da855ac6419e10458b1a9926a0873a61148
Aditya <adityanivarthi19@gmail.com>
2015-10-16 10:10:02 -0400

    Added a list initialization to smart sort plan. Commented plan creation in exploit sort query planner because of incorrect arguments to merge sort plan.

--- src/simpledb/materialize/SmartSortPlan.java
+++ src/simpledb/materialize/SmartSortPlan.java
@@ -1,10 +1,14 @@
 package simpledb.materialize;
 
-import simpledb.tx.Transaction;
-import simpledb.record.*;
-import simpledb.query.*;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.util.*;
+import simpledb.query.Plan;
+import simpledb.query.Scan;
+import simpledb.query.UpdateScan;
+import simpledb.record.Schema;
+import simpledb.record.TableInfo;
+import simpledb.tx.Transaction;
 
 /**
  * The Smart Plan class for the <i>sort</i> operator.
@@ -14,20 +18,24 @@ public class SmartSortPlan extends AbstractSortPlan {
 
     /**
      * Creates a sort plan for the specified query.
-     * @param p the plan for the underlying query
-     * @param sortfields the fields to sort by
-     * @param tx the calling transaction
+     *
+     * @param p
+     *            the plan for the underlying query
+     * @param sortfields
+     *            the fields to sort by
+     * @param tx
+     *            the calling transaction
      */
     public SmartSortPlan(TableInfo ti, Plan p, List<String> sortfields, Transaction tx) {
         super(p, sortfields, tx);
 
         this.ti = ti;
     }
-   
+
     /**
-     * This method is where most of the action is.
-     * Up to 2 sorted temporary tables are created,
-     * and are passed into SortScan for final merging.
+     * This method is where most of the action is. Up to 2 sorted temporary
+     * tables are created, and are passed into SortScan for final merging.
+     *
      * @see simpledb.query.Plan#open()
      */
     @Override
@@ -35,8 +43,8 @@ public class SmartSortPlan extends AbstractSortPlan {
         Boolean sorted = ti.getSorted();
 
         Scan src = p.open();
-        List<TempTable> runs;
-        // If the table is already sorted, it isn't necessary to sort again. 
+        List<TempTable> runs = new ArrayList<TempTable>();
+        // If the table is already sorted, it isn't necessary to sort again.
         if (!sorted) {
             runs = splitIntoRuns(src);
             while (runs.size() > 2) {
@@ -50,20 +58,19 @@ public class SmartSortPlan extends AbstractSortPlan {
             }
         }
         src.close();
-        this.ti.setSorted(true);
+        ti.setSorted(true);
 
         // Write temp tables generated to the file blocks
         // get some kind of page.write()
 
         return new SmartSortScan(runs, comp);
     }
-   
+
     /**
-     * Returns the number of blocks in the sorted table,
-     * which is the same as it would be in a
-     * materialized table.
-     * It does <i>not</i> include the one-time cost
-     * of materializing and sorting the records.
+     * Returns the number of blocks in the sorted table, which is the same as it
+     * would be in a materialized table. It does <i>not</i> include the one-time
+     * cost of materializing and sorting the records.
+     *
      * @see simpledb.query.Plan#blocksAccessed()
      */
     @Override
@@ -72,38 +79,40 @@ public class SmartSortPlan extends AbstractSortPlan {
         Plan mp = new MaterializePlan(p, tx); // not opened; just for analysis
         return mp.blocksAccessed();
     }
-   
+
     /**
-     * Returns the number of records in the sorted table,
-     * which is the same as in the underlying query.
+     * Returns the number of records in the sorted table, which is the same as
+     * in the underlying query.
+     *
      * @see simpledb.query.Plan#recordsOutput()
      */
     @Override
     public int recordsOutput() {
         return p.recordsOutput();
     }
-   
+
     /**
-     * Returns the number of distinct field values in
-     * the sorted table, which is the same as in
-     * the underlying query.
+     * Returns the number of distinct field values in the sorted table, which is
+     * the same as in the underlying query.
+     *
      * @see simpledb.query.Plan#distinctValues(java.lang.String)
      */
     @Override
     public int distinctValues(String fldname) {
         return p.distinctValues(fldname);
     }
-   
+
     /**
-     * Returns the schema of the sorted table, which
-     * is the same as in the underlying query.
+     * Returns the schema of the sorted table, which is the same as in the
+     * underlying query.
+     *
      * @see simpledb.query.Plan#schema()
      */
     @Override
     public Schema schema() {
         return sch;
     }
-   
+
     @Override
     protected List<TempTable> splitIntoRuns(Scan src) {
         List<TempTable> temps = new ArrayList<TempTable>();
@@ -120,13 +129,13 @@ public class SmartSortPlan extends AbstractSortPlan {
                 currentscan.close();
                 currenttemp = new TempTable(sch, tx);
                 temps.add(currenttemp);
-                currentscan = (UpdateScan) currenttemp.open();
+                currentscan = currenttemp.open();
             }
         }
         currentscan.close();
         return temps;
     }
-   
+
     @Override
     protected List<TempTable> doAMergeIteration(List<TempTable> runs) {
         List<TempTable> result = new ArrayList<TempTable>();
@@ -140,7 +149,7 @@ public class SmartSortPlan extends AbstractSortPlan {
         }
         return result;
     }
-   
+
     @Override
     protected TempTable mergeTwoRuns(TempTable p1, TempTable p2) {
         Scan src1 = p1.open();
@@ -157,7 +166,7 @@ public class SmartSortPlan extends AbstractSortPlan {
                 hasmore2 = copy(src2, dest);
             }
         }
-        
+
         if (hasmore1) {
             while (hasmore1) {
                 hasmore1 = copy(src1, dest);
@@ -172,7 +181,7 @@ public class SmartSortPlan extends AbstractSortPlan {
         dest.close();
         return result;
     }
-   
+
     @Override
     protected boolean copy(Scan src, UpdateScan dest) {
         dest.insert();
